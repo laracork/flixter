a6b20568a7a85bb41ac9e7b4f42d01daa470f4ec
Lara Corkrey <laracorkrey@gmail.com>
2018-04-25 02:57:54 +0000

    updated changing lessons position

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'static_pages#index'
+  root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-  resources :courses, only: [:index, :show]
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :lessons, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
