7308faae024709ad49c4e738f60441316376a2b3
Lara Corkrey <laracorkrey@gmail.com>
2018-04-27 03:21:41 +0000

    updated sections reordering

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
+
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+    resources :sections, only: [:update]
+
+    resources :sections, only: [] do 
+      resources :lessons, only: [:create, :show, :edit, :destroy]
     end
+
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
